6d1c39f52caa7a7cc8f6c87418d56d6f99e2cb72
xavier1 <xavier1@eu.nus.edu>
2021-12-20 14:05:59 +0800

    add LQR gains

--- scripts/pen.py
+++ scripts/pen.py
@@ -56,6 +56,10 @@ class TaskManager:
         self.local_position = PoseStamped()
         self.attitude_sp = PoseStamped()
         self.pen_pose = PoseStamped()
+        self.quad_vel = TwistStamped() #quadrotor velocity from filtered vicon
+        self.quad_pose = PoseStamped()
+        self.relative_speed = TwistStamped()
+        self.pen_vel = TwistStamped() #pendulum velocity from filtered vicon
         self.state = State()
         self.local_velocity = TwistStamped()  # local_velocity initialize
         self.attitude_rate = AttitudeTarget()  # use for attitude setpoints pub
@@ -79,18 +83,22 @@ class TaskManager:
             'mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=1)
         self.attitude_thrust_pub = rospy.Publisher(
             'mavros/setpoint_attitude/thrust', Thrust, queue_size = 1)
+        self.relative_speed_pub = rospy.Publisher('relative_speed', TwistStamped, queue_size = 1)
         # ROS subscribers
 
         self.local_pos_sub = rospy.Subscriber(
             'mavros/local_position/pose', PoseStamped, self.local_position_callback)
+        self.quad_pos_sub = rospy.Subscriber('mavros/vision_pose/pose',PoseStamped, self.quad_pos_callback)
         self.state_sub = rospy.Subscriber(
             'mavros/state', State, self.state_callback)
         self.cmd_sub = rospy.Subscriber('user/cmd', Byte, self.cmd_callback)
         self.vel_sub = rospy.Subscriber('mavros/local_position/velocity_local',
                                         TwistStamped, self.local_velocity_callback)  # local_velocity susbcriber
+        self.quad_vel_sub = rospy.Subscriber('quadrotor_vel_filtered', TwistStamped, self.quad_vel_callback)
+        self.pen_vel_sub = rospy.Subscriber('pendulum_vel_filtered',TwistStamped, self.pen_vel_callback)
         #self.vel_global_sub = rospy.Subscriber('mavros/local_position/velocity_local', TwistStamped, self.global_velocity_callback)
         # send setpoints in seperate thread to better prevent failsafe
-        self.pen_pose_sub = rospy.Subscriber('/vrpn_client_ros/pen/pose', PoseStamped, self.pen_position_callback)
+        self.pen_pose_sub = rospy.Subscriber('pendulum_pos_filtered', PoseStamped, self.pen_position_callback)
         self.pos_thread = Thread(target=self.send_pos_ctrl, args=())
         self.pos_thread.daemon = True
         self.pos_thread.start()
@@ -114,6 +122,16 @@ class TaskManager:
 
     def local_velocity_callback(self, data):  # local_velocity callback
         self.local_velocity = data
+
+    def quad_pos_callback(self, data):
+        self.quad_pos = data
+
+    def quad_vel_callback(self, data):
+        self.quad_vel = data
+        print("subbed filtered quad_vel")
+
+    def pen_vel_callback(self, data):
+        self.pen_vel = data
 
     def send_pos(self):
         rate = rospy.Rate(10)
@@ -184,6 +202,7 @@ class TaskManager:
         self.state = data
     def pen_position_callback(self, data):
         self.pen_pose = data
+        #self.pen_vel = data
     #
     # Helper methods
     #
@@ -250,30 +269,80 @@ if __name__ == '__main__':
     uavTask.pos.pose.position.x = 0
     uavTask.pos.pose.position.y = 0
     uavTask.pos.pose.position.z = 0
+    dX0 = 0
+    dY0 = 0
+    dx0 = 0
+    dy0 = 0
 
     uavTask.set_mode("OFFBOARD", 5)
     uavTask.set_arm(True, 5)
+    #xr_hat = np.array([[0,0]]).T
+    #xs_hat = np.array([[0,0]]).T
+    dt = 0.01
+
+    #gamma = 0
+    #beta = 0
+    dvx_filter = 0
+    dvy_filter = 0
+    dvz_filter = 0
+    cutoff_freq = 10
 
     while not rospy.is_shutdown():
-        rate = rospy.Rate(200)
+        rate = rospy.Rate(100)
         #print(uavTask.task_state)
         # uavTask.position_pub.publish(uavTask.pos)
+        if uavTask.task_state == uavTaskType.Idle:
+            quad_vx = uavTask.quad_vel.twist.linear.x
+            pen_vx = uavTask.pen_vel.twist.linear.x
+            dvx = pen_vx - quad_vx
+            #px = uavTask.pen_pose.pose.position.x
+            #dx = px - x
+            #print("pendulum x is",px)
+            #print("quadrotor x is", x)
+            print("relative vx is", dvx)
+            uavTask.pos.pose.position.x = 0
+            uavTask.pos.pose.position.y = 0
+            uavTask.pos.pose.position.z = 0
+            uavTask.position_pub.publish(uavTask.pos)
         if uavTask.task_state == uavTaskType.TakeOff:
             #rospy.loginfo("Doing LQR takeoff")
-            uavTask.pos_sp = [0, 0, 0.5]
+            uavTask.pos_sp = [0, 0, 0.75]
             # Get position feedback from PX4
             x = uavTask.local_position.pose.position.x
+            #x = uavTask.quad_pose.pose.position.x
             #print("current p_x is {0}".format(x))
             y = uavTask.local_position.pose.position.y
+            #y = uavTask.quad_pose.pose.position.y
             #print("curent p_y is {0}".format(y))
             z = uavTask.local_position.pose.position.z  # ENU used in ROS
+            #z = uavTask.quad_pose.pose.position.z
+            quad_vx = uavTask.quad_vel.twist.linear.x
+            quad_vy = uavTask.quad_vel.twist.linear.y
+            quad_vz = uavTask.quad_vel.twist.linear.z
+            #filter_a = dt/(dt + 1/(2*3.14*cutoff_freq))
+            #dvx_filter = (1-filter_a)*dvx_filter + filter_a * quad_vx
+            #dvy_filter = (1-filter_a)*dvy_filter + filter_a * quad_vy
+            #dvz_filter = (1-filter_a)*dvz_filter + filter_a * quad_vz
+
             vx_enu = uavTask.local_velocity.twist.linear.x  # NWU body frame
             vy_enu = uavTask.local_velocity.twist.linear.y
             vz_enu = uavTask.local_velocity.twist.linear.z
             # LQR-based controller, x-gamma, y-beta, z-alpha
             # gamma = uavTask.euler[0]
             # beta = uavTask.euler[1]
-
+            #dx = uavTask.pen_pose.pose.position.x - x
+            #dvx = (dx - dx0)/0.01
+            #dx0 = dx
+            #print("relative vx is",dvx)
+            #print("previous dx0 is",dx0)
+            #dy = uavTask.pen_pose.pose.position.y - y
+            #dvy = (dy - dy0)/0.01
+            #dy0 = dy
+            #print("relative vy is",dvy)
+            #uavTask.relative_speed.twist.linear.x = dvx
+            #uavTask.relative_speed.twist.linear.y = dvy
+            #if (dvx != 0 and dvy !=0):
+            #   uavTask.relative_speed_pub.publish(uavTask.relative_speed)
             yaw = 0/57.3 # attitude_rate setpoint body_z
             # yaw = 0 #simulation face east
             state_x = np.array([[x, vx_enu]]).T
@@ -286,20 +355,22 @@ if __name__ == '__main__':
             gamma = -np.matmul(K_y, state_y) # attitude setpoint body_x
             state_z = np.array([[z-uavTask.pos_sp[2], vz_enu]]).T
             # K_z = np.array([[0.7071, 1.2305]])
-            K_z = np.array([[1,1.7321]]) #less aggresive
-            a = -np.matmul(K_z, state_z)/(3*9.8)+0.39 #throttle sp
+            K_z = np.array([[3.1623,2.8852]]) #less aggresive
+            a = -np.matmul(K_z, state_z)/(3*9.8)+0.34 #throttle sp
             #a = float(a)
 
             uavTask.attitude_rate.body_rate = Vector3()
             uavTask.attitude_rate.header = Header()
             uavTask.attitude_rate.header.frame_id = "base_footprint"
-            #uavTask.attitude_rate.orientation = 
+            #uavTask.attitude_rate.orientation =
             quat = quaternion_from_euler(gamma, beta, yaw)
-            print("current gamma is {0}".format(gamma))
+            #print("current gamma is {0}".format(gamma))
             #print("current beta is {0}".format(beta))
             #quat = quaternion_from_euler(0, 0, 0)
-            # uavTask.attitude_rate.body_rate.y = 0
-            # uavTask.attitude_rate.body_rate.z = 0
+            dX = uavTask.pen_pose.pose.position.x - x
+            dvx = uavTask.pen_vel.twist.linear.x - vx_enu
+            uavTask.attitude_rate.body_rate.y = dX
+            uavTask.attitude_rate.body_rate.z = dvx
             #eu = np.array([[gamma, beta, yaw]]).T
             #quat = quaternion_from_euler(gamma, beta, yaw) # X,Y,Z,W
             #uavTask.attitude_rate.orientation = quat
@@ -324,24 +395,41 @@ if __name__ == '__main__':
             #uavTask.pos_control_pub.publish(uavTask.position)
 
         elif uavTask.task_state == uavTaskType.Mission:
-            rospy.loginfo("Flying pendulum")
-            uavTask.pos_sp = [0, 0, 0.5]
+            #rospy.loginfo("Flying pendulum")
+            #Ar = np.array([[0, 1],[9.81, 0]])
+            #As = Ar
+            #Br = np.array([[0, -9.81]]).T
+            #Bs = np.array([[0, 9.81]]).T
+            #L = np.array([[30, 9.81+200]]).T #estimator gain
+            uavTask.pos_sp = [0, 0, 0.7]
             # Get position feedback from PX4
             # uavTask.pen_pose.pose.position.x
             x = uavTask.local_position.pose.position.x
             y = uavTask.local_position.pose.position.y
             z = uavTask.local_position.pose.position.z  # ENU used in ROS
             dX = uavTask.pen_pose.pose.position.x - x
-            threshold = 0.00
-            if abs(dX) >= threshold:
-               dx = dX
-            else:
-               dx = 0
+            quad_vx = uavTask.local_velocity.twist.linear.x
+            pen_vx = uavTask.pen_vel.twist.linear.x
+            dvx = pen_vx - quad_vx
+            #dxr_hat = np.matmul(Ar,xr_hat) + Br*beta + L * (dX-xr_hat[0,0])
+            #dvx = (dX - dX0)/0.01
+            #dX0 = dX
+            #threshold = 0.00
+            #if abs(dX) >= threshold:
+            #   dx = dX
+            #else:
+            #   dx = 0
             dY = uavTask.pen_pose.pose.position.y - y
-            if abs(dY) >= threshold:
-               dy = dY
-            else:
-               dy = 0
+            quad_vy = uavTask.local_velocity.twist.linear.y
+            pen_vy = uavTask.pen_vel.twist.linear.y
+            dvy = pen_vy - quad_vy
+            #dxs_hat = np.matmul(As,xs_hat) + Bs*gamma + L*(dY-xs_hat[0,0])
+            #dvy = (dY - dY0) / 0.01
+            #dY0 = dY
+            #if abs(dY) >= threshold:
+            #   dy = dY
+            #else:
+            #   dy = 0
             #dx = 0
             #dy = 0
             vx_enu = uavTask.local_velocity.twist.linear.x  # NWU body frame
@@ -353,18 +441,47 @@ if __name__ == '__main__':
 
             yaw = 0/57.3 # attitude_rate setpoint body_z
             # yaw = 0 #simulation face east
-            state_x = np.array([[dx, 0, x, vx_enu]]).T
+            state_x = np.array([[dX, dvx, x, vx_enu]]).T
+            #xr_hat = xr_hat + dt * dxr_hat
             # K_x = np.array([[0.1,0.1724]]) heading East!!!
-            K_x = np.array([[-4.5601, -1.0495, -0.01, -0.0858]]) #less aggressive
+            #LQR gains
+            #R = 200, Q_pen= 400
+            #K_x = np.array([[-4.3285, -1.9256, -0.0707, -0.2303]])
+            #R = 100
+            #K_x = np.array([[-3.3041,-1.0597,-0.1,-0.2388]])
+            #R = 1000
+            #K_x = np.array([[-2.6048, -0.8326, -0.0316, -0.1066]])
+            #R = 200
+            #K_x = np.array([[-3.02, -0.967, -0.0707, -0.1848]]) #less aggressive
+            # R = 200, Q_pen = 10
+            #K_x = np.array([[-3.0713, -1.0037, -0.0707,-0.1868]])
+            # R = 200, Q_pen = 100 better performance
+            #K_x = np.array([[-3.4785, -1.2976, -0.0707, -0.2019]])
+            # R=200, Q_pen= 200 best
+            K_x = np.array([[-3.8147, -1.5437, -0.0707, -0.2136]])
             beta = -np.matmul(K_x, state_x) # attitude setpoint body_y
-            state_y = np.array([[dy, 0, y, vy_enu]]).T
+            state_y = np.array([[dY, dvy, y, vy_enu]]).T
+            #xs_hat = xs_hat + dt * dxs_hat
             # K_y = np.array([[-0.1, -0.1724])
-            K_y = np.array([[4.5601, 1.0495, 0.01, 0.0858]])
+            # R=200 Q_pen = 400 oscillate a lot
+            #K_y = np.array([[4.3285, 1.9256, 0.0707, 0.2303]])
+            # R=100
+            #K_y = np.array([[3.3041, 1.0597, 0.1, 0.2388]])
+            # R = 1000
+            #K_y = np.array([[2.6048, 0.8326, 0.0316, 0.1066]])
+            # R = 200
+            #K_y = np.array([[3.02, 0.967, 0.0707, 0.1848]])
+            # R = 200, Q_pen = 10
+            #K_y = np.array([[3.0713, 1.0037, 0.0707, 0.1868]])
+            # R = 200, Q_pen = 100
+            #K_y = np.array([[3.4785, 1.2976, 0.0707, 0.2019]])
+            #R= 200, Q_pen = 200 best (oscillate when battery low?)
+            K_y = np.array([[3.8147,1.5437,0.0707,0.2136]])
             gamma = -np.matmul(K_y, state_y) # attitude setpoint body_x
             state_z = np.array([[z-uavTask.pos_sp[2], vz_enu]]).T
             # K_z = np.array([[0.7071, 1.2305]])
-            K_z = np.array([[1,1.7321]]) #less aggresive
-            a = -np.matmul(K_z, state_z)/(3*9.8)+0.37 #throttle sp
+            K_z = np.array([[3.1623,2.8852]]) #less aggresive
+            a = -np.matmul(K_z, state_z)/(3*9.8)+0.36 #throttle sp
             #a = float(a)
 
             uavTask.attitude_rate.body_rate = Vector3()
@@ -373,8 +490,8 @@ if __name__ == '__main__':
             #uavTask.attitude_rate.orientation = 
             quat = quaternion_from_euler(gamma, beta, yaw)
             #quat = quaternion_from_euler(0, 0, 0)
-            # uavTask.attitude_rate.body_rate.y = 0
-            # uavTask.attitude_rate.body_rate.z = 0
+            uavTask.attitude_rate.body_rate.y = dX
+            uavTask.attitude_rate.body_rate.z = dvx
             #eu = np.array([[gamma, beta, yaw]]).T
             #quat = quaternion_from_euler(gamma, beta, yaw) # X,Y,Z,W
             #uavTask.attitude_rate.orientation = quat
